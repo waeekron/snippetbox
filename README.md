9c0103cdd47c2b0ffcdfabb574c261a5e21e4f32
waeekron <waeekron@LAPTOP-BJADCT21.localdomain>
2024-01-20 03:43:43 +0200

    progress

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 Follow along practice project from Let's Go book by Alex Edwards.
+
+page: 144
--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -1,9 +1,44 @@
 package main
 
-import "github.com/waeekron/snippetbox/internal/models"
+import (
+	"github.com/waeekron/snippetbox/internal/models"
+	"path/filepath"
+)
 
 // Holding structure for any dynamic data passed down to a template
 type templateData struct {
 	Snippet  *models.Snippet
 	Snippets []*models.Snippet
+}
+
+func newTemplateCache() (map[string]*template.Template, error) {
+	// init cache
+	cache := map[string]*template.Template{}
+
+	pages, err := filepath.Glob("./ui/html/pages/*.tmpl")
+	if err != nil {
+		return nil, err
+	}
+	for _, page := range pages {
+		// Extract the file name (like 'home.tmpl') from the full filepath
+		// and assign it to the name variable.
+		name := filepath.Base(page)
+		// Create a slice containing the filepaths for our base template, any
+		// partials and the page.
+		files := []string{
+			"./ui/html/base.tmpl",
+			"./ui/html/partials/nav.tmpl",
+			page,
+		}
+		// Parse the files into a template set.
+		ts, err := template.ParseFiles(files...)
+		if err != nil {
+			return nil, err
+		}
+		// Add the template set to the map, using the name of the page
+		// (like 'home.tmpl') as the key.
+		cache[name] = ts
+	}
+
+	return cache, nil
 }
